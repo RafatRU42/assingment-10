b233170d3d34b1f8ffe39872d3d4c434593378cc
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2022-10-25 20:47:55 +0600

    auth config start

--- src/components/AuthProvider/AuthProvider.js
+++ src/components/AuthProvider/AuthProvider.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import {getAuth} from 'firebase/auth'
+import app from '../../firebase.config';
+
+
+export const authContext = createContext();
+const auth = getAuth(app)
 
 const AuthProvider = () => {
     return (
---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtoeFkdBGxqeQuk4afHqQ-df8SMMWqxoM",
+  authDomain: "learning-point-client-1058c.firebaseapp.com",
+  projectId: "learning-point-client-1058c",
+  storageBucket: "learning-point-client-1058c.appspot.com",
+  messagingSenderId: "653420787868",
+  appId: "1:653420787868:web:6a641a648e2f56300e5559"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
