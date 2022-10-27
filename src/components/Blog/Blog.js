2aa9017ce7da0da33280dd50b54e04c18f3d29d0
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2022-10-27 11:41:01 +0600

    environment code hided

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,10 +4,28 @@ import Header from '../Header/Header';
 const Blog = () => {
     return (
         <div>
-        <Header></Header>
+            <Header></Header>
 
-            <h1>This is Blog</h1>
+            <h3>1. What is CORS ?</h3>
+            <h3>Ans.  CORS is the mechanism that provides the ability to alter the behavior of this policy, enabling you to do things like hosting static content</h3>
+            <hr />
+            <h3>2.Why You use Firebase ? </h3>
+            <h3>Ans. Firebase is an authentication system app. It is easy to apply and much secure. The alternatives of firebase is MongoDB, Orcle Database etc.</h3>
+            <hr />
+            <h3>3.How dees private routes work?</h3>
+            <h3>Ans. Private Routes vary based on the Apps, For example, Dashboard, User Profile, App Settings, Home etc. In simple words, These routes can be accessed only after login.
+
+                The constraints for Public and Private routes are that Public routes should not be accessed after login and Private routes should not be accessible before login.</h3>
+                <hr />
+                <h3>4.what is Node ? How does Node works? </h3>
+                <h3>Ans. Node.js is an open source server environment.
+It is free.
+Node.js runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.).Node.js can generate dynamic page content.
+It can create, open, read, write, delete, and close files on the server</h3>
+
         </div>
+       
+
     );
 };
 
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCtoeFkdBGxqeQuk4afHqQ-df8SMMWqxoM",
-  authDomain: "learning-point-client-1058c.firebaseapp.com",
-  projectId: "learning-point-client-1058c",
-  storageBucket: "learning-point-client-1058c.appspot.com",
-  messagingSenderId: "653420787868",
-  appId: "1:653420787868:web:6a641a648e2f56300e5559"
+  apiKey: process.env.REACT_APP_apikey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
